98c043d49e127b81802973e343a9f2272657b68e
shanwaz-78 <111566952+shanwaz-78@users.noreply.github.com>
2023-07-07 12:11:02 +0530

    Update remove duplicate.js

--- Array Problems/Easy/remove duplicate.js
+++ Array Problems/Easy/remove duplicate.js
@@ -1,10 +1,12 @@
 function removeDuplicate(arr) {
-  let newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== arr[i + 1]) {
-      newArr.push(arr[i]);
-    }
-  }
-  return newArr;
+    const seen = new Set();
+    const duplicate = new Set();
+    for (let i = 0; i < arr.length; i++) {
+        if (seen.has(arr[i])) {
+            duplicate.add(arr[i]);
+        };
+        seen.add(arr[i])
+    };
+    return Array.from(duplicate)
 }
 console.log(removeDuplicate([1, 1, 1, 2, 2, 4, 5, 5, 6]));
