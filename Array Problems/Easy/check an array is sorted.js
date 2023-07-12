0a480d490f36708a66b748939743171d973838dc
shanwaz-78 <111566952+shanwaz-78@users.noreply.github.com>
2023-07-12 13:11:10 +0530

    Update check an array is sorted.js

--- Array Problems/Easy/check an array is sorted.js
+++ Array Problems/Easy/check an array is sorted.js
@@ -1,10 +1,10 @@
 function isSorted(arr) {
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > arr[i + 1]) {
-      return false;
+    if (arr[i] < arr[i + 1]) {
+      return true;
     }
   }
-  return true;
+  return false;
 }
 isSorted([1, 2, 3, 4]); // -> true
 isSorted([4, 3, 2, 1]); // -> false
